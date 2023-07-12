0779d6247d6cd432d78da674c81ede7cac3c6281
Liam-WB <>
2023-07-12 16:56:35 +0000

    -Add temp comments, code -Add function names for structure

--- run.py
+++ run.py
@@ -18,10 +18,30 @@ SS = GSPREAD_CLIENT.open("VENUE-booker-ss")
 
 # TEMP CODE - Shows that api and Google sheet are connected
 
-large_venues = SS.worksheet("large_venues")
+venues = SS.worksheet("venues")
 
-large_venues_data = large_venues.get_all_values()
+venues_data = venues.get_all_values()
 
-print(large_venues_data)
+print(venues_data)
 
 # NEXT - Add efficient functions, dict() method with zip() to create dictionaries for each page of SS
+
+# TO CHECK vv // Issue with gspread?
+
+def welcome():
+    # Provide user with venue category option
+    welcome_selection = input("Welcome!\n Please select which task you would like to execute:\n 1. Update a venue booking\n 2. Display venue maximum seats\n 3. Display venue current booked seats\n")
+    if (welcome_selection == 1):
+        collect_data()
+    elif (welcome_selection == 2):
+        display_max_seats()
+    elif (welcome_selection == 3):
+        display_current_seats()
+    else:
+        return "ERROR: Please provide a valid entry"
+
+def collect_data():
+
+def display_max_seats():
+
+def display_current_seats():
