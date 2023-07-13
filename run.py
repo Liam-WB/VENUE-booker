41ba5d8fd010dc0088262e97855b0f343e451836
Liam-WB <>
2023-07-13 14:17:10 +0000

    -Add temp code, placeholder function names -Add main program functions, collect data function, correct data function

--- run.py
+++ run.py
@@ -30,7 +30,7 @@ print(venues_data)
 
 def welcome():
     # Provide user with venue category option
-    welcome_selection = input("Welcome!\n Please select which task you would like to execute:\n 1. Update a venue booking\n 2. Display venue maximum seats\n 3. Display venue current booked seats\n")
+    welcome_selection = input("Please select which task you would like to execute:\n 1. Update a venue booking\n 2. Display venue maximum seats\n 3. Display venue current booked seats\n")
     if (welcome_selection == 1):
         collect_data()
     elif (welcome_selection == 2):
@@ -38,10 +38,61 @@ def welcome():
     elif (welcome_selection == 3):
         display_current_seats()
     else:
-        return "ERROR: Please provide a valid entry"
+        raise ValueError(
+                f"ERROR: {welcome_selection} is not a valid entry.")
 
 def collect_data():
+    # User provides correct data & type
+    # While loop
+    while True:
+        print("Please provide the updated seats\n In the format:\n X, X, X, X, X, X, X, X, X")
+        seats_input = input("Provide the data below: \n")
 
-def display_max_seats():
+        seats_list = list(seats_input.split(","))
+
+        if correct_seats(seats_list):
+            break
+
+    return seats_list
+
+def correct_data(seats):
+    # Checks if data provided is correct
+    # Try statement checks for correct number of data
+    try:
+        [int(seat) for seat in seats]
+        if len(values) != 9:
+            raise ValueError(
+                f"9 values are required - You submitted {len(seats)} values.")
+
+    except ValueError as e:
+        print(f"Invalid submission: {e}, please try again\n")
+        return False
+
+    return True
+
+
+def general_functions():
+    # For all general program functions
+    seats = collect_data()
+    seats_list = [int(i) for i in seats]
+    # To add
+
+# To add worksheet update functions
+
+def display_max_seats(seats):
+    # Display a dictionary containing venue & data lists
+    venue_list = [x for x in SS.worksheet("VENUE-booker-ss").row_values(1)]
+    sseats_dictionary = dict(zip(venue_list, seats))
+    return seats_dictionary
+    
+    # To be changed
+    stock_values = get_stock_values(stock_data)
+    print(stock_values)
+    
 
 def display_current_seats():
+    # To be added
+
+print("Welcome to VENUE booker!")
+# line to be added
+
