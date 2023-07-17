efa53f3bb90aa5a8af4fc110e102cc175ff4bb3f
Liam-WB <>
2023-07-17 16:20:29 +0000

    -Add display function code -Remove unneeded strings from program

--- run.py
+++ run.py
@@ -43,7 +43,7 @@ def correct_welcome(datas):
             print(f"\nYou have selected {datas}")
         else:
             raise ValueError(
-                f"ERROR: {datas} is not a valid entry. Please submit a value from 1 - 4")
+                f"{datas} is not a valid entry. Please submit a value from 1 - 4")
 
     except ValueError as e:
         print(f"ERROR: {e}, Please try again\n")
@@ -84,21 +84,48 @@ def correct_data(seats):
 
 # DISPLAY f's SECTION
 
-def display_row_values():
+def display_row_values(display):
     # Display the specified row values and return a dictionary with venue names & booking amounts
     # last_row_updated = venues page last updated row (found by length of the column values)
     venues = SS.worksheet("venues").row_values(1)
     last_row_updated = SS.worksheet("venues").row_values(len(SS.worksheet("venues").col_values(1)))
+    last_five_rows =  SS.worksheet("venues").row_values((len(SS.worksheet("venues").col_values(1))-3))
 
-def display_selection():
-    # Input for what specific information user wants to display
+    if display == "1":
+        return last_row_updated
+    elif display == "2":
+        return last_five_rows
+
+def collect_display():
+    # Input for what specific (validated) information user wants to display
     while True:
         display_option = input("\nPlease select which information you would like to view:\n \n 1. Last updated row\n 2. Last 5 updated rows\n 3. All spreadsheet values\n 4. Custom / Specific row\n \n")
-        
-        if correct_display_value():
+        display = display_option
+
+        if correct_display(display):
             break
 
     return display_option
+
+def correct_display(display):
+    try:
+        if display == "1":
+            print(f"\nYou have selected {display}")
+        elif display == "2":
+            print(f"\nYou have selected {display}")
+        elif display == "3":
+            print(f"\nYou have selected {display}")
+        elif display == "4":
+            print(f"\nYou have selected {display}")
+        else:
+            raise ValueError(
+                f"{display} is not a valid entry. Please submit a value from 1 - 4")
+
+    except ValueError as e:
+        print(f"ERROR: {e}, Please try again\n")
+        return False
+
+    return True
 
 # UPDATE SECTION
 
@@ -116,19 +143,22 @@ def update_SS(next_row, worksheet):
 
 def general_functions():
 
-    # For all general program f's
+    # MAIN
 
-    # Welcome section f's
+    # WELCOME f SECTION
     datas = collect_welcome()
     if datas == "1":
         # Data collection / validation f's
         seats = collect_data()
         seats_list = [int(i) for i in seats]
-
-    # UPDATE f CALLING SECTION
-
-    # Spreadsheet update function
-    update_SS(seats_list, "VENUE-booker-ss")
+        # UPDATE f CALLING SECTION
+        # Spreadsheet update function
+        update_SS(seats_list, "VENUE-booker-ss")
+    elif datas == "2" or "3" or "4":
+        # DISPLAY f SECTION
+        display = collect_display()
+    if datas == "2":
+            display_row_values(display)
 
 # RUN PROGRAM
 
